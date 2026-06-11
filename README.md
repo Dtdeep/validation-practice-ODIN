e05ba5bf199b17ffc8fd8a48f7852af5f23aa165
Dtdeep <dumagandesthiny@gmail.com>
2026-06-11 11:19:30 +0800

    Add proper validation messages for each inputs

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# webpack-template
+## HTML
+
+email, country,postal code,password and confirm password.
+Each are inside a p element then inside a label element so
+everything it goes to the input when the texts are clicked.
+two span elements one for what the input is about and another on the bottom on the rules of the input elements and its error.
+use the html validation guide sa itermediate html css.
+
+email should be well email.
+country is a select, zip code is type text with philippine postal code validation,japan postal code and indonesia. password should contain one uppercase, one number, one special character and at least 7 characters long with a maximum of 20 characters. confirm password's validation should only be the password and confirm password input to be the same.
+
+## javascript validation
+
+Each inputs has their own error message + praise message objects
+
+1. email :
+   `Email should be a proper Email "abc@example.com"`
+   `email is required`
+   `That is a good email`.
+2. Country no validation lmao.
+3. postal code: just use existing postal code stuff.
+   `Philippine Postal Code should be 4 digits`
+   `Postal code is required`
+   `Japanese Postal Code should be 7 digits in the format NNN-NNNN`
+   `Indonesian Postal Code should be 5 digits`
+   `Valid Postal Code Like Emoji`
+
+4.password :
+`Password should countain 1 special character, 1 uppercase and 1number at leaset 7 characters.`
+`password is required`
+`Password should contain 1 special character`
+`Password should contain 1 uppercase`
+`Password should contain 1 number`
+`password should be at least 7 characters`
+`password sohuld be no more than 20 characters`
+
+`Good Password`
+
+5. `confirm-password is not the same as the password above`
+   `Confirm Passsword is required`
+   `Good Password`
+
+each input elements except country should have their own event listener with their own objects. Everything should be required.
+Default message should be shown when the user first enters the page. The listener should trigger every typed character in each input and message should change accordingly.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,181 @@
 import "./styles.css";
+
+const emailInput = document.querySelector("#email-input");
+const countryInput = document.querySelector("#country-input");
+const postalInput = document.querySelector("#postal-input");
+const passwordInput = document.querySelector("#password-input");
+const confirmPasswordInput = document.querySelector("#confirm-password-input");
+const submitButton = document.querySelector("#submit-btn");
+
+const messageEmail = document.querySelector(".message.email");
+const messagePostal = document.querySelector(".message.postal");
+const messagePassword = document.querySelector(".message.password");
+const messageConfirmPassword = document.querySelector(
+  ".message.confirm-password",
+);
+
+const regUpper = new RegExp("^(?=.*[A-Z]).+$", "");
+const regSpecial = new RegExp("^(?=.*[^A-Za-z0-9]).+$", "");
+const regNumber = new RegExp("^(?=.*\\d).+$");
+const regCharacters = new RegExp("^.{7,}$");
+const messages = {
+  upper: "at least 1 upper case character, ",
+  special: "at least 1 special character, ",
+  number: "at least 1 number, ",
+  characters: "at least 7 characters",
+  good: "Good Password",
+};
+
+const validateEmail = () => {
+  const emailMessageError = {
+    default: "Email should be a proper email 'abc@example.com'",
+    good: "That is a good email",
+  };
+
+  if (emailInput.validity.typeMismatch || emailInput.validity.valueMissing) {
+    messageEmail.textContent = emailMessageError.default;
+  } else if (emailInput.validity.valid) {
+    messageEmail.textContent = emailMessageError.good;
+  }
+};
+const validatePostalCode = () => {
+  const postalMessageError = {
+    ph: ["^\\d{4}$", `Philippine Postal Code should be 4 digits`],
+    jp: [
+      "^\\d{3}-\\d{4}$",
+      `Japanese Postal Code should be 7 digits in the format NNN-NNNN`,
+    ],
+    id: ["^\\d{5}$", `Indonesian Postal Code should be 5 digits`],
+  };
+
+  const selectedCountry = countryInput.value;
+  console.log(selectedCountry);
+  const constraint = new RegExp(postalMessageError[selectedCountry][0], "");
+
+  if (!constraint.test(postalInput.value)) {
+    messagePostal.textContent = postalMessageError[selectedCountry][1];
+    postalInput.setCustomValidity("1");
+  } else {
+    messagePostal.textContent = "Valid Postal Code 👍";
+    postalInput.setCustomValidity("");
+  }
+};
+
+const validatePassword = () => {
+  let message = "Password should contain ";
+
+  if (!regUpper.test(passwordInput.value)) {
+    message += messages.upper;
+    passwordInput.setCustomValidity("1");
+  }
+
+  if (!regSpecial.test(passwordInput.value)) {
+    message += messages.special;
+    passwordInput.setCustomValidity("1");
+  }
+
+  if (!regNumber.test(passwordInput.value)) {
+    message += messages.number;
+    passwordInput.setCustomValidity("1");
+  }
+
+  if (!regCharacters.test(passwordInput.value)) {
+    message += messages.characters;
+    passwordInput.setCustomValidity("1");
+  }
+
+  if (
+    regUpper.test(passwordInput.value) &&
+    regSpecial.test(passwordInput.value) &&
+    regNumber.test(passwordInput.value) &&
+    regCharacters.test(passwordInput.value)
+  ) {
+    message = "Good Password";
+    passwordInput.setCustomValidity("");
+  }
+  messagePassword.textContent = message;
+
+  validateConfirmPassword();
+};
+
+const validateConfirmPassword = () => {
+  let message = "Password should contain ";
+
+  if (!regUpper.test(confirmPasswordInput.value)) {
+    message += messages.upper;
+    confirmPasswordInput.setCustomValidity("1");
+  }
+
+  if (!regSpecial.test(confirmPasswordInput.value)) {
+    message += messages.special;
+    confirmPasswordInput.setCustomValidity("1");
+  }
+
+  if (!regNumber.test(confirmPasswordInput.value)) {
+    message += messages.number;
+    confirmPasswordInput.setCustomValidity("1");
+  }
+
+  if (!regCharacters.test(confirmPasswordInput.value)) {
+    message += messages.characters;
+    confirmPasswordInput.setCustomValidity("1");
+  }
+  messageConfirmPassword.textContent = message;
+
+  if (
+    regUpper.test(passwordInput.value) &&
+    regSpecial.test(passwordInput.value) &&
+    regNumber.test(passwordInput.value) &&
+    regCharacters.test(passwordInput.value) &&
+    passwordInput.value === confirmPasswordInput.value
+  ) {
+    messageConfirmPassword.textContent = `Good Password`;
+    confirmPasswordInput.setCustomValidity("");
+  } else if (
+    passwordInput.value !== confirmPasswordInput.value &&
+    !confirmPasswordInput
+  ) {
+    confirmPasswordInput.setCustomValidity("1");
+    messageConfirmPassword.textContent =
+      "confirm-password is not the same as the password above";
+  }
+};
+
+const validateSubmit = (event) => {
+  if (emailInput.validity.valueMissing) {
+    messageEmail.textContent = "Email is required";
+  }
+
+  if (postalInput.validity.valueMissing) {
+    messagePostal.textContent = "Postal is required";
+  }
+
+  if (passwordInput.validity.valueMissing) {
+    messagePassword.textContent = "Password is required";
+  }
+
+  if (confirmPasswordInput.validity.valueMissing) {
+    messageConfirmPassword.textContent = "Confirm-password is required";
+  }
+
+  if (
+    confirmPasswordInput.validity.valueMissing ||
+    passwordInput.validity.valueMissing ||
+    postalInput.validity.valueMissing ||
+    emailInput.validity.valueMissing
+  ) {
+    event.preventDefault();
+  }
+};
+
+submitButton.addEventListener("click", validateSubmit);
+
+validateEmail();
+validatePostalCode();
+validatePassword();
+validateConfirmPassword();
+emailInput.addEventListener("input", validateEmail);
+countryInput.addEventListener("change", validatePostalCode);
+postalInput.addEventListener("input", validatePostalCode);
+passwordInput.addEventListener("input", validatePassword);
+confirmPasswordInput.addEventListener("input", validatePassword);
